8807b643b65a2b7926d52a673f30eb13755600a5
freifunkkt <freifunk@awi.me>
2016-07-20 00:15:28 +0200

    Update falterturm.nix

--- hosts/falterturm.nix
+++ hosts/falterturm.nix
@@ -51,18 +51,19 @@ in
     externalInterface = "enp0s3";
     ip4Interfaces = [ "tun0" "enp0s3" ];
     ip6Interface = "heipv6";
-    ip6Tunnel = "
+    ip6Tunnel = ''
       modprobe ipv6
       ip tunnel add heipv6 mode sit remote 216.66.86.114 local 188.68.56.228 ttl 255
       ip link set heipv6 up
       ip addr add 2001:470:6c:83::2/64 dev heipv6
       ip route add ::/0 dev he-ipv6
-    ";
+    '';
     
     #ist das die normale Konfiguration der Schnittstelle
     networkingLocalCommands = ''
       ip rule add from 188.68.56.228/32 lookup 5
       ip route replace default via 188.68.56.3 table 5
+      
     '';
     
     graphite = secrets.stats.servername;
